9602d18fc53398fd4ac0e11f6811706e5362f095
LaurenRina <laurenkateryna@gmail.com>
2022-09-01 14:59:41 +0300

    added the js

---
+++ src/app.js
@@ -0,0 +1,78 @@
+function formatDate(timestamp) {
+  let now = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Satday",
+  ];
+  let day = days[now.getDay()];
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  let min = now.getMinutes();
+  if (min < 10) {
+    min = "0" + min;
+  }
+  let months = [
+    "Junuary",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+  let date = now.getDate();
+  let text = `${day} ${hours}:${min}, ${month} ${date}`;
+  return text;
+}
+
+function showTemperature(response) {
+  let temperature = document.querySelector("#current-temperature");
+  let city = document.querySelector("#city");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let date = document.querySelector("#date");
+  let icon = document.querySelector("#icon");
+
+  temperature.innerHTML = response.data.main.temp.toFixed(0);
+  city.innerHTML = response.data.name;
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  date.innerHTML = formatDate(response.data.dt * 1000);
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+
+function search(city) {
+  let apiKey = "3e5761385c02293899defe61082c2901";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(`${apiUrl}`).then(showTemperature);
+}
+
+function submit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  search = `${searchInput.value}`;
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", submit);
+
+let celsiusTemperature = null;
+
+search("Kyiv");
